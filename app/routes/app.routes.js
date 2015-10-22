c7af536def120d42e8f5f2e3515c64dd9d2be808
Brian Brennan <brianb@cardsandpockets.com>
2015-10-22 12:33:53 -0400

    added view animation

--- app/routes/app.routes.js
+++ app/routes/app.routes.js
@@ -4,10 +4,14 @@ var route = angular.module('app.routes', ['ngRoute']);
 
 route.config(function($routeProvider, $locationProvider){
 	$routeProvider.when('/', {
-		templateUrl: 'app/views/home.html',
+		templateUrl: 'app/views/home.html'
 	})
 
 	.when('/about',{
 		templateUrl: 'app/views/about.html'
+	})
+
+	.when('/skills',{
+		templateUrl: 'app/views/skills.html'
 	});
 });

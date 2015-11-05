9746148f78c32de3aed27c0e7ff77adf752c225c
Brian Brennan <webintern@cardsandpockets.com>
2015-11-05 16:53:58 -0500

    not fixed ehader, non analytic

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,6 @@
 angular.module('mainCtrl', [])
 
-	.controller('mainController', function($scope, $anchorScroll, $rootScope){
+	.controller('mainController', function($scope, $anchorScroll, $rootScope, $window, $location){
 		// $scope.loading = true;
 		// setTimeout(function(){
 		// 	$scope.loading = false;
@@ -13,25 +13,6 @@ angular.module('mainCtrl', [])
 
 			$scope.viewLoading = true;
 
-			if(next != 'undefined'){
-				if(next.$$route.originalPath == '/'){
-					s('header').class('dark');
-
-					document.addEventListener('scroll', function(){
-						var height = Number(s('.hero').css('height')[0].substr(0, s('.hero').css('height')[0].length - 2));
-
-						if(document.body.scrollTop > height){
-							s('header').removeClass('dark');
-						} else {
-							s('header').class('dark');
-						}
-					});
-
-				} else {
-					s('header').class('');
-				}
-			}
-
 		});
 
 		$scope.$on('$routeChangeSuccess', function(event, next, current){
@@ -43,6 +24,37 @@ angular.module('mainCtrl', [])
 		});
 
 		$rootScope.$on('$routeChangeSuccess', function(event, next, current){
+
+			function listener(event){
+				if(typeof s('hero').css('height')[0] !== 'undefined'){
+					var height = Number(s('.hero').css('height')[0].substr(0, s('.hero').css('height')[0].length - 2));
+				
+					if(document.body.scrollTop > height){
+						s('header').removeClass('dark');
+					} else {
+						s('header').class('dark');
+					}
+				}
+			}
+
+			console.log($location.path());
+
+			if($location.path() === '/'){
+				s('header').class('dark');
+
+				document.addEventListener('scroll', listener);
+
+			} else {
+				s('header').class('');
+				document.removeEventListener('scroll', listener);
+			}
+			
+			if(!$window.ga){
+				console.log('oops');
+				return;
+			}
+			$window.ga('send', 'pageview', { page: $location.path() });
+
 			$rootScope.loading = false;
 		});
 		

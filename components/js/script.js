c3825c8027fc75a88fcfbd46a0a7f8f1b2770826
Brian Brennan <me@brianbrennan.com>
2016-02-01 02:27:45 -0500

    starting some changes

--- components/js/script.js
+++ components/js/script.js
@@ -19,6 +19,30 @@ S.ready(function(){
 	//---------------------Pre tag styling
 
 	
+	//---------------------Navigation for Mobile
 
+	S.ready(function(){
+	s('.nav-toggle').on('click', function(){
+		if(window.innerWidth <= 780){
+			if(!s('.nav-list').hasClass('active')[0])
+				s('.nav-list').addClass('active');
+			else
+				s('.nav-list').removeClass('active');
+			if(!s(this).hasClass('active')[0])
+				s(this).addClass('active');
+			else
+				s(this).removeClass('active');
+		}
+
+	});
+
+	s('.nav-list a').on('click', function(){
+		if(window.innerWidth <= 780){
+			s('.nav-list').removeClass('active');
+			s('.nav-toggle').removeClass('active');
+		}
+	});
+
+});
 
 });
